c77c6910e6e10fb76d19d6011e2ea419d5d582bb
meetIgor <iigoor.s@gmail.com>
2022-09-13 13:28:38 +0300

    Initial Commit

---
+++ ColorizeApp/ColorizeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizeAppApp.swift
+//  ColorizeApp
+//
+//  Created by igor s on 13.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorizeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
