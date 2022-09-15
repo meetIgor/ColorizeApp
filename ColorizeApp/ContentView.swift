6670b5cc579ec92fd9f3780c49d246e647132bd8
meetIgor <iigoor.s@gmail.com>
2022-09-15 13:36:57 +0300

    Finished. corrections after feedback

--- ColorizeApp/ContentView.swift
+++ ColorizeApp/ContentView.swift
@@ -17,14 +17,21 @@ struct ContentView: View {
     @State private var green = Double.random(in: 0...255).rounded()
     @State private var blue = Double.random(in: 0...255).rounded()
     
-    @State private var showAlert = false
-    
     @FocusState private var focusField: FocusFiled?
+    
+    let color = CGColor(red: 41/255, green: 128/255, blue: 185/255, alpha: 1)
     
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.black, .blue]), startPoint: .top, endPoint: .bottom)
-                .ignoresSafeArea()
+            LinearGradient(
+                gradient: Gradient(colors: [.black, Color(color)]),
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .ignoresSafeArea()
+            .onTapGesture {
+                focusField = nil
+            }
             
             VStack(spacing: 50) {
                 Color(red: red/255, green: green/255, blue: blue/255)
@@ -33,23 +40,13 @@ struct ContentView: View {
                     .overlay(RoundedRectangle(cornerRadius: 20).stroke(.gray, lineWidth: 4))
                 
                 VStack {
-                    HStack {
-                        ColorSliderView(value: $red, sliderColor: .red, textColor: .white)
-                        TextFieldView(value: $red)
-                            .focused($focusField, equals: .red)
-                    }
-                    HStack {
-                        ColorSliderView(value: $green, sliderColor: .green, textColor: .white)
-                        TextFieldView(value: $green)
-                            .focused($focusField, equals: .green)
-                        
-                    }
-                    HStack {
-                        ColorSliderView(value: $blue, sliderColor: .blue, textColor: .white)
-                        TextFieldView(value: $blue)
-                            .focused($focusField, equals: .blue)
-                        
-                    }
+                    ColorSliderView(value: $red, sliderColor: .red)
+                        .focused($focusField, equals: .red)
+                    ColorSliderView(value: $green, sliderColor: .green)
+                        .focused($focusField, equals: .green)
+                    ColorSliderView(value: $blue, sliderColor: .blue)
+                        .focused($focusField, equals: .blue)
+                    
                 }
                 .keyboardType(.numberPad)
                 .toolbar {
@@ -61,19 +58,19 @@ struct ContentView: View {
                             Image(systemName: "chevron.down")
                         }
                         Spacer()
-                        Button("Done", action: checkValue)
-                            .alert("Wrong Format", isPresented: $showAlert, actions: {}) {
-                                Text("Please enter value from 0 to 255")
-                            }
+                        Button("Done") {
+                            focusField = nil
+                        }
                     }
                 }
-                
                 Spacer()
-                
             }
             .padding()
         }
     }
+}
+
+extension ContentView {
     
     private func focusedUp() {
         switch focusField {
@@ -84,9 +81,10 @@ struct ContentView: View {
         case .blue:
             focusField = .green
         case .none:
-            return
+            focusField = nil
         }
     }
+    
     private func focusDown() {
         switch focusField {
         case .red:
@@ -96,69 +94,14 @@ struct ContentView: View {
         case .blue:
             focusField = .red
         case .none:
-            return
+            focusField = nil
         }
     }
     
-    private func checkValue() {
-        switch focusField {
-        case .some(.red):
-            if !(0...255).contains(red) {
-                showAlert.toggle()
-                return
-            }
-        case .green:
-            if !(0...255).contains(green) {
-                showAlert.toggle()
-                return
-            }
-        case .blue:
-            if !(0...255).contains(blue) {
-                showAlert.toggle()
-                return
-            }
-        case .none:
-            return
-        }
-        
-        focusField = nil
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
-}
-
-struct ColorSliderView: View {
-    @Binding var value: Double
-    let sliderColor: Color
-    let textColor: Color
-    
-    var body: some View {
-        HStack {
-            Text(value, format: .number)
-                .foregroundColor(textColor)
-                .frame(width: 40)
-                .font(.system(size: 18))
-            
-            Slider(value: $value, in: 0...255, step: 1)
-                .tint(sliderColor)
-            
-        }
-    }
-}
-
-struct TextFieldView: View {
-    @Binding var value: Double
-    
-    var body: some View {
-        TextField("", value: $value, format: .number)
-            .frame(width: 50)
-            .textFieldStyle(.roundedBorder)
-            .multilineTextAlignment(.center)
-            .font(.system(size: 18))
-    }
-    
 }
